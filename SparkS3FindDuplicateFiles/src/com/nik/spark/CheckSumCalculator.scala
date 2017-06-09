9f6fb360b61aaf7e5f47f4e941e1b847dfa182a0
nikhilbhide <nikhilbhide15@gmail.com>
2017-06-09 12:10:24 +0300

    Added proper comments and refactored code to improve readbility

--- SparkS3FindDuplicateFiles/src/com/nik/spark/CheckSumCalculator.scala
+++ SparkS3FindDuplicateFiles/src/com/nik/spark/CheckSumCalculator.scala
@@ -8,33 +8,38 @@ import org.apache.spark.rdd.RDD.rddToOrderedRDDFunctions
 import scala.Iterator
 
 /**
- * Calculates checksum for supplied RDD. Check sum is figured out by calculating md5 hash of RDD.
+ * Calculates checksum of supplied RDD
  */
 class CheckSumCalculator(rdd: RDD[String]) extends Serializable {
 
+  /**
+   * Calculates checksum for supplied RDD. Check sum is figured out by calculating md5 hash of RDD.
+   * @param rdd The rdd of S3 file
+   * @return The checksum of S3 file contents
+   */
   def calculateCheckSum(parts: Int = 1000): String = {
-    val partitioner = new HashPartitioner(parts) 
-    val output = rdd.
-               map(x => (x, 1)).
-               repartitionAndSortWithinPartitions(partitioner).
-               map(_._1).
-               mapPartitions(x => Iterator(x.foldLeft(getMd5Digest())(md5))).
-               map(x => new java.math.BigInteger(1, x.digest()).toString(16)).
-               collect().
-               sorted.
-               foldLeft(getMd5Digest())(md5)
+    val partitioner = new HashPartitioner(parts)
+    val output = rdd
+      .map(x => (x, 1))
+      .repartitionAndSortWithinPartitions(partitioner)
+      .map(x => (x._1))
+      .mapPartitions(x => Iterator(x.foldLeft(getMd5Digest())(md5)))
+      .map(x => new java.math.BigInteger(1, x.digest()).toString(16))
+      .collect()
+      .sorted
+      .foldLeft(getMd5Digest())(md5)
     val checksum = new java.math.BigInteger(1, output.digest()).toString(16)
-    return(checksum)
+    return (checksum)
   }
 
- /**
-  * @param prev
-  * @param in2
-  * @return
-  */
-  def md5(prev: MessageDigest, next: String): MessageDigest = {
-    val b = next.getBytes("UTF-8")
-    prev.update(b, 0, b.length)
-    prev
+  /**
+   * @param currMD5 The MD5 value of file contents so far read
+   * @param input The next string in
+   * @return The updated MD5 value
+   */
+  def md5(currMD5: MessageDigest, input: String): MessageDigest = {
+    val inputBytes = input.getBytes("UTF-8")
+    currMD5.update(inputBytes, 0, inputBytes.length)
+    currMD5
   }
 }
